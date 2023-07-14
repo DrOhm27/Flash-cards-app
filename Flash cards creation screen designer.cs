0351c9b03bfa7addd6e87b02d555bc651e814cf9
Ohm <129056363+DrOhm27@users.noreply.github.com>
2023-07-14 03:12:13 +0200

    Flash cards creation screen designer
    The code for "Flash cards creation screen designer".

---
+++ Flash cards creation screen designer.cs
@@ -0,0 +1,515 @@
+namespace Flash_cards_app
+{
+    partial class Flash_cards_creation_screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            groupBox3 = new GroupBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            groupBox4 = new GroupBox();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            groupBox5 = new GroupBox();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            groupBox6 = new GroupBox();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            groupBox7 = new GroupBox();
+            textBox7 = new TextBox();
+            label8 = new Label();
+            groupBox8 = new GroupBox();
+            textBox8 = new TextBox();
+            label9 = new Label();
+            groupBox9 = new GroupBox();
+            textBox9 = new TextBox();
+            label6 = new Label();
+            groupBox10 = new GroupBox();
+            textBox10 = new TextBox();
+            label10 = new Label();
+            groupBox11 = new GroupBox();
+            textBox11 = new TextBox();
+            label11 = new Label();
+            groupBox12 = new GroupBox();
+            textBox12 = new TextBox();
+            label12 = new Label();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            groupBox11.SuspendLayout();
+            groupBox12.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.Location = new Point(22, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 119);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Flash card 1";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(188, 24);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(67, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 16);
+            label1.TabIndex = 1;
+            label1.Text = "Question:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox2.Location = new Point(269, 22);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 119);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Flash card 2";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(6, 89);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(188, 24);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(67, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 16);
+            label2.TabIndex = 0;
+            label2.Text = "Answer:";
+            label2.Click += label2_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox6);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox3.Location = new Point(535, 22);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(200, 119);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Flash card 3";
+            groupBox3.Enter += groupBox3_Enter;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(6, 39);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(188, 24);
+            textBox3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(73, 20);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 16);
+            label3.TabIndex = 0;
+            label3.Text = "Question:";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(textBox8);
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(label9);
+            groupBox4.Controls.Add(label8);
+            groupBox4.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox4.Location = new Point(797, 22);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(200, 119);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Flash card 4";
+            groupBox4.Enter += groupBox4_Enter;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(6, 89);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(188, 24);
+            textBox4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(73, 67);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 16);
+            label4.TabIndex = 0;
+            label4.Text = "Answer:";
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(textBox10);
+            groupBox5.Controls.Add(textBox9);
+            groupBox5.Controls.Add(label10);
+            groupBox5.Controls.Add(label6);
+            groupBox5.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox5.Location = new Point(22, 186);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(200, 116);
+            groupBox5.TabIndex = 4;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Flash card 5";
+            groupBox5.Enter += groupBox5_Enter;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(6, 39);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(188, 24);
+            textBox5.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(73, 20);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 16);
+            label5.TabIndex = 0;
+            label5.Text = "Question:";
+            // 
+            // groupBox6
+            // 
+            groupBox6.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox6.Location = new Point(269, 186);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(200, 116);
+            groupBox6.TabIndex = 5;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Flash card 6";
+            groupBox6.Enter += groupBox6_Enter;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(6, 85);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(188, 24);
+            textBox6.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(73, 66);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 16);
+            label7.TabIndex = 0;
+            label7.Text = "Answer:";
+            // 
+            // groupBox7
+            // 
+            groupBox7.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox7.Location = new Point(535, 186);
+            groupBox7.Name = "groupBox7";
+            groupBox7.Size = new Size(200, 116);
+            groupBox7.TabIndex = 6;
+            groupBox7.TabStop = false;
+            groupBox7.Text = "Flash card 7";
+            groupBox7.Enter += groupBox7_Enter;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(6, 39);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(188, 24);
+            textBox7.TabIndex = 1;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(74, 20);
+            label8.Name = "label8";
+            label8.Size = new Size(63, 16);
+            label8.TabIndex = 0;
+            label8.Text = "Question:";
+            // 
+            // groupBox8
+            // 
+            groupBox8.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox8.Location = new Point(797, 186);
+            groupBox8.Name = "groupBox8";
+            groupBox8.Size = new Size(200, 116);
+            groupBox8.TabIndex = 7;
+            groupBox8.TabStop = false;
+            groupBox8.Text = "Flash card 8";
+            groupBox8.Enter += groupBox8_Enter;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(6, 85);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(188, 24);
+            textBox8.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(74, 66);
+            label9.Name = "label9";
+            label9.Size = new Size(54, 16);
+            label9.TabIndex = 0;
+            label9.Text = "Answer:";
+            // 
+            // groupBox9
+            // 
+            groupBox9.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox9.Location = new Point(22, 345);
+            groupBox9.Name = "groupBox9";
+            groupBox9.Size = new Size(200, 100);
+            groupBox9.TabIndex = 8;
+            groupBox9.TabStop = false;
+            groupBox9.Text = "Flash card 9";
+            groupBox9.Enter += groupBox9_Enter;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(6, 39);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(188, 24);
+            textBox9.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(67, 20);
+            label6.Name = "label6";
+            label6.Size = new Size(63, 16);
+            label6.TabIndex = 0;
+            label6.Text = "Question:";
+            // 
+            // groupBox10
+            // 
+            groupBox10.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox10.Location = new Point(269, 345);
+            groupBox10.Name = "groupBox10";
+            groupBox10.Size = new Size(200, 100);
+            groupBox10.TabIndex = 9;
+            groupBox10.TabStop = false;
+            groupBox10.Text = "Flash card 10";
+            groupBox10.Enter += groupBox10_Enter;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(6, 85);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(188, 24);
+            textBox10.TabIndex = 1;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(67, 66);
+            label10.Name = "label10";
+            label10.Size = new Size(54, 16);
+            label10.TabIndex = 0;
+            label10.Text = "Answer:";
+            // 
+            // groupBox11
+            // 
+            groupBox11.Controls.Add(textBox11);
+            groupBox11.Controls.Add(label11);
+            groupBox11.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox11.Location = new Point(535, 345);
+            groupBox11.Name = "groupBox11";
+            groupBox11.Size = new Size(200, 100);
+            groupBox11.TabIndex = 10;
+            groupBox11.TabStop = false;
+            groupBox11.Text = "Flash card 11";
+            groupBox11.Enter += groupBox11_Enter;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(52, 63);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(111, 24);
+            textBox11.TabIndex = 1;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(52, 44);
+            label11.Name = "label11";
+            label11.Size = new Size(111, 16);
+            label11.TabIndex = 0;
+            label11.Text = "Please enter text";
+            // 
+            // groupBox12
+            // 
+            groupBox12.Controls.Add(textBox12);
+            groupBox12.Controls.Add(label12);
+            groupBox12.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox12.Location = new Point(797, 345);
+            groupBox12.Name = "groupBox12";
+            groupBox12.Size = new Size(200, 100);
+            groupBox12.TabIndex = 11;
+            groupBox12.TabStop = false;
+            groupBox12.Text = "Flash card 12";
+            groupBox12.Enter += groupBox12_Enter;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(53, 63);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(111, 24);
+            textBox12.TabIndex = 1;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(53, 44);
+            label12.Name = "label12";
+            label12.Size = new Size(111, 16);
+            label12.TabIndex = 0;
+            label12.Text = "Please enter text";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(436, 451);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 53);
+            button1.TabIndex = 12;
+            button1.Text = "Start the quiz!";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Flash_cards_creation_screen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 516);
+            Controls.Add(button1);
+            Controls.Add(groupBox12);
+            Controls.Add(groupBox11);
+            Controls.Add(groupBox10);
+            Controls.Add(groupBox9);
+            Controls.Add(groupBox8);
+            Controls.Add(groupBox7);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Flash_cards_creation_screen";
+            Text = "Flash_cards_creation_screen";
+            Load += Flash_cards_creation_screen_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            groupBox11.ResumeLayout(false);
+            groupBox11.PerformLayout();
+            groupBox12.ResumeLayout(false);
+            groupBox12.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Label label2;
+        private GroupBox groupBox3;
+        private Label label3;
+        private GroupBox groupBox4;
+        private Label label4;
+        private GroupBox groupBox5;
+        private Label label5;
+        private GroupBox groupBox6;
+        private Label label7;
+        private GroupBox groupBox7;
+        private Label label8;
+        private GroupBox groupBox8;
+        private Label label9;
+        private GroupBox groupBox9;
+        private Label label6;
+        private GroupBox groupBox10;
+        private Label label10;
+        private GroupBox groupBox11;
+        private Label label11;
+        private GroupBox groupBox12;
+        private Label label12;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+    }
+}
